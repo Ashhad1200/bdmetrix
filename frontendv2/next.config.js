03392be9bf6e213c7545958ad753ad37c934fb3b
ashhad <syedashhad17@gmail.com>
2026-01-05 04:29:41 +0500

    style: add blank lines to next.config.js.

--- frontendv2/next.config.js
+++ frontendv2/next.config.js
@@ -63,6 +63,10 @@ const nextConfig = {
               "frame-ancestors 'self'",
             ].join('; ')
           },
+
+
+
+          
           {
             key: 'Cache-Control',
             value: 'public, max-age=31536000, immutable',
